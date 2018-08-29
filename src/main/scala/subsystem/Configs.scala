978eb604c8069d9b3fc011bf24bdfbfff0f7fc2c
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2018-08-29 10:41:10 +0100

    Experiment with larger cache

--- src/main/scala/subsystem/Configs.scala
+++ src/main/scala/subsystem/Configs.scala
@@ -41,10 +41,16 @@ class WithNBigCores(n: Int) extends Config((site, here, up) => {
         divEarlyOut = true))),
       dcache = Some(DCacheParams(
         rowBits = site(SystemBusKey).beatBits,
-        nMSHRs = 0,
+        nSets = 64,
+        nWays = 16,
+        nTLBEntries = 64,
+        nMSHRs = 1,
         blockBytes = site(CacheBlockBytes))),
       icache = Some(ICacheParams(
         rowBits = site(SystemBusKey).beatBits,
+        nSets = 64,
+        nWays = 8,
+        nTLBEntries = 64,
         blockBytes = site(CacheBlockBytes))))
     List.tabulate(n)(i => big.copy(hartId = i))
   }
