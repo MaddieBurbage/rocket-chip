cdc876fe9b87ff25b2376d2388c11490086c5d3e
MaddieBurbage <madeline.g.burbage@gmail.com>
2021-05-13 15:46:24 -0400

    Updated the dancing links accelerator for the new mem system

--- src/main/scala/tile/DancingLinks.scala
+++ src/main/scala/tile/DancingLinks.scala
@@ -7,6 +7,7 @@ import Chisel._
 import freechips.rocketchip.config._ //For Config
 import freechips.rocketchip.diplomacy._ //For LazyModule
 import freechips.rocketchip.rocket.{HellaCacheReq} //For cache accesses
+import chisel3.VecInit
 
 // Wrapper for the accelerator
 class DancingLinks()(implicit p: Parameters) extends LazyRoCC {
@@ -17,11 +18,13 @@ class DancingLinks()(implicit p: Parameters) extends LazyRoCC {
 class DancingLinksImp(outer: DancingLinks)(implicit p: Parameters) extends LazyRoCCModule(outer) {
 
   //State control
-  val memState = DancingLinksImp.memFSM(io.mem.req.fire(), io.mem.resp.valid)
-  val doneWithMem = io.mem.resp.valid && (memState === DancingLinksImp.m_finish)
+  val faulted = io.mem.s2_xcpt.asUInt.orR
+  val memState = DancingLinksImp.memFSM(io.mem.req.fire(), io.mem.resp.valid, faulted)
+  val doneWithMem = ((io.mem.resp.valid || faulted) && (memState === DancingLinksImp.m_finish1 || memState === DancingLinksImp.m_fix)) || (faulted && memState === DancingLinksImp.m_wait)
   val dancingFired = Wire(Bool())
   val setupFired = Wire(Bool())
-  val state = DancingLinksImp.FSM(dancingFired, setupFired, doneWithMem, io.resp.fire())
+  
+  val state = DancingLinksImp.FSM(dancingFired, setupFired || doneWithMem, io.resp.fire())
 
   // Instruction data
   val setupInst = io.cmd.bits.inst.funct(1) //Setup or Dancing function
@@ -29,26 +32,27 @@ class DancingLinksImp(outer: DancingLinks)(implicit p: Parameters) extends LazyR
   setupFired := io.cmd.fire() && setupInst
   val cover = Reg(init = Bool(true)) // Whether to cover or uncover the element
   val rd = Reg(init = io.cmd.bits.inst.rd)
+  val exceptions = Reg(init=0.U(6.W))
+  val fault_code = io.mem.s2_xcpt.asUInt | exceptions
+  exceptions := fault_code
 
   //RoCC Interface
   val structWidth = Reg(init = 16.U(64.W)) //Bytes of width for the element struct
   io.interrupt := Bool(false)
-  io.cmd.ready := (state === DancingLinksImp.s_idle)
-  io.busy := (state === DancingLinksImp.s_busy)
-  io.resp.valid := (state === DancingLinksImp.s_resp)
-  io.resp.bits.data := structWidth //No rd needed yet
+  io.cmd.ready := state === DancingLinksImp.s_idle && memState === DancingLinksImp.m_load
+  io.busy := state =/= DancingLinksImp.s_idle //change to not idle when you can!!!
+  io.resp.valid := state === DancingLinksImp.s_resp
+  io.resp.bits.data := fault_code //Use non-zero response to signify a page fault error
   io.resp.bits.rd := rd
 
   // Setup data
-  val nameArray = Reg(init = 0.U(64.W))
-  //val structWidth = Reg(init = 16.U(64.W)) //Bytes of width for the element struct
-
-  // Dancing Link data
+  val nameArray = Reg(init = 0.U(64.W)) //Index to base of the array of structs
   val elementIndex = Reg(init = io.cmd.bits.rs1) //Index to current struct
-
+  
   // Instruction arrives
   when(io.cmd.fire()) {
     rd := io.cmd.bits.inst.rd
+    exceptions := 0.U
     when(setupInst) { //Setting up the accelerator
       nameArray := io.cmd.bits.rs1
       structWidth := io.cmd.bits.rs2
@@ -58,13 +62,11 @@ class DancingLinksImp(outer: DancingLinks)(implicit p: Parameters) extends LazyR
     }
   }
 
-  // First element's addresses
+  // Base address for the elements
   val offsetAddress = nameArray + 0.U //Base address, aligned to skip nothing
 
   // Read address:
   val elementAddress = offsetAddress + elementIndex * structWidth
-
-  //val elementSecond = elementAddress + 4.U //Indices are ints, so skipping one is 4 bytes
 
   // Neighbor elements' addresses
   val firstNeighborIndex = Reg(init = 0.U(32.W))
@@ -77,7 +79,8 @@ class DancingLinksImp(outer: DancingLinks)(implicit p: Parameters) extends LazyR
   val secondNeighborChange = secondNeighborAddress + 0.U //Change 1st index
 
   // mem interface setup
-  io.mem.req.valid := (state === DancingLinksImp.s_busy) && (memState != DancingLinksImp.m_wait) && (memState != DancingLinksImp.m_finish)
+  val reqState = (memState === DancingLinksImp.m_load) || (memState === DancingLinksImp.m_storeFirst) || (memState === DancingLinksImp.m_storeSecond)
+  io.mem.req.valid := state === DancingLinksImp.s_busy && reqState
   io.mem.req.bits.phys := Bool(false)
 
   when(memState === DancingLinksImp.m_load) { //Read from the element
@@ -105,61 +108,82 @@ class DancingLinksImp(outer: DancingLinks)(implicit p: Parameters) extends LazyR
       io.mem.req.bits.tag := Cat(1.U, secondNeighborChange(4,0))
     }
   }
-
 }
 
 object DancingLinksImp {
   val s_idle :: s_busy :: s_resp :: Nil = Enum(Bits(), 3)
-  val m_load :: m_wait :: m_storeFirst :: m_storeSecond :: m_finish :: Nil = Enum(Bits(), 5)
+  val m_load :: m_wait :: m_storeFirst :: m_storeSecond :: m_finish1 :: m_finish2 :: m_fix :: Nil = Enum(Bits(), 7)
 
-  def FSM(gotInst: Bool, didSetup: Bool, finishedMem: Bool, sentResp: Bool): UInt = {
+  def FSM(moveLinks: Bool, finishedTask: Bool, sentResp: Bool): UInt = {
     // Accelerator states
-    val state = Reg(init = DancingLinksImp.s_idle) //Idle until handling an instruction
+    val state = Reg(init = s_idle) //Idle until handling an instruction
 
     //State transitions
-    when(gotInst) {
-      state := DancingLinksImp.s_busy
+    when(moveLinks) {
+      state := s_busy
       printf("State now busy\n")
     }
 
-    when(finishedMem || didSetup) {
-      state := DancingLinksImp.s_resp
+    when(finishedTask) {
+      state := s_resp
       printf("State now responding\n")
     }
 
     when(sentResp) {
-      state := DancingLinksImp.s_idle
+      state := s_idle
       printf("State now idle\n")
     }
 
     state
   }
 
-  def memFSM(sentReq: Bool, gotResp: Bool): UInt = {
+  def memFSM(sentReq: Bool, gotResp: Bool, faulted: Bool): UInt = {
     // Memory states
-    val mem_state = Reg(init = DancingLinksImp.m_load)
+    val mem_state = Reg(init = m_load)
+    val storesLeft = Reg(init = 0.U(2.W))
+    val currentStoresLeft = storesLeft + sentReq - gotResp - faulted
+    storesLeft := currentStoresLeft
 
-    when(sentReq) {
-      when(mem_state === m_storeFirst) {
-        mem_state := m_storeSecond
-        printf("Mem state storing second\n")
-      } .elsewhen(mem_state === m_load) {
-        mem_state := m_wait
-        printf("Mem state waiting for a load\n")
+    when(mem_state === m_load && sentReq) {
+      mem_state := m_wait
+      printf("Mem state waiting for a load\n")
+    }
+    when(mem_state === m_wait && gotResp) {
+      mem_state := m_storeFirst
+      printf("Mem state storing first\n")
+    }
+    when(mem_state === m_storeFirst && sentReq) {
+      mem_state := m_storeSecond
+      printf("Mem state storing second\n")
+    }
+    when(mem_state === m_storeSecond && sentReq) {
+      when(currentStoresLeft === 1.U) {
+        mem_state := m_finish1
+      } .otherwise {
+        mem_state := m_finish2
       }
+      printf("Mem state finishing stores\n")
+    }
+    when(mem_state === m_finish2 && gotResp) {
+      mem_state := m_finish1
+      printf("Mem state finishing store\n")
+    }
+    when(mem_state === m_finish1 && gotResp) {
+      mem_state := m_load
+      printf("Mem state loading\n")
     }
 
-    when(gotResp) {
+    when(mem_state =/= m_fix && faulted) {
       when(mem_state === m_wait) {
-        mem_state := m_storeFirst
-        printf("Mem state storing first\n")
-      } .elsewhen(mem_state === m_storeSecond) {
-        mem_state := m_finish
-        printf("Mem state waiting for next response\n")
-      } .elsewhen(mem_state === m_finish) {
         mem_state := m_load
-        printf("Mem state loading\n")
+      } .otherwise {
+        mem_state := m_fix
       }
+      printf("Mem failed\n")
+    }
+    when(mem_state === m_fix && currentStoresLeft === 0.U) {
+      mem_state := m_load
+      printf("Mem state waiting for a load\n")
     }
 
     mem_state
